16314def236619fc154e079c739385df6fec508d
jenish patel <jenishpatel.me@gmail.com>
2021-06-10 11:55:53 -0400

    recipe list logic added

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../recipe.model';
 
 @Component({
   selector: 'app-recipe-list',
@@ -6,7 +7,10 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
-
+  recipe: Recipe[] = [
+    new Recipe("Recipe One", "Recipe description goes here ", "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSzRuawcuNyvX_0CL3dHx-fOzkBrXqscCoSUA&usqp=CAU"),
+    new Recipe("Recipe Two", "Recipe description goes here ", "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRwhuDBEM9fk2g8n435rxK5Uk6hVLOMOfgr9A&usqp=CAU")
+  ];
   constructor() { }
 
   ngOnInit(): void {
